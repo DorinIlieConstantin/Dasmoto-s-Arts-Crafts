8c9aed1c109a0218b0e54c28ae430d446d07e700
DorinIlieConstantin <66999645+DorinIlieConstantin@users.noreply.github.com>
2020-06-17 08:24:52 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+const refrigerator = {
+    dairy: ['cheese', 'milk', 'sour cream'],
+    temperature: 35,
+    'produce drawer': {
+      vegetables: ['lettuce', 'broccoli', 'peas'],
+      fruit: ['apples', 'berries', 'grapes'] 
+    }
+  };
